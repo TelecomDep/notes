32e82b7d0bb31c69bd38c445e818d94a11d5d34c
fzybot <ruslan.akhpashev@gmail.com>
2025-07-22 09:03:56 +0300

    check .css style

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,9 +27,25 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+source_suffix = ['.rst', '.md']
+
+# The master toctree document.
+master_doc = 'index'
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+def setup(app):
+   app.add_css_file("stylesheet.css")
+
