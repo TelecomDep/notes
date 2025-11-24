d172464d5f57db0abf23ccfdd706cd29350f4a3c
fzybot <ruslan.akhpashev@gmail.com>
2025-11-24 10:03:47 +0300

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ sys.path.append('.')
 Tells Sphinx which extensions to use.
 """
 extensions = [
-    'jupyterbook_patches',
+    'myst_nb',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
